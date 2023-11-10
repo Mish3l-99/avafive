ff2e00d89dc5b4909216020d2637856c61a28b8f
Mish3l <meshaal.nasor10@gmail.com>
2023-11-10 08:23:00 +0400

    first

--- components/molecules/CartProducts.tsx
+++ components/molecules/CartProducts.tsx
@@ -17,51 +17,60 @@ const CartProducts = () => {
         <div className="col-span-2 paragraph-m-16 text-typeLow">Qty</div>
         <div className="col-span-2 paragraph-m-16 text-typeLow">Subtotal</div>
       </div>
-      {products.map((prod, i) => {
-        const { title, brand, price, thumbnail } = prod;
-        return (
-          <div
-            key={i}
-            className="my-2 border-b mb-2 pb-10 md:pb-2 grid grid-cols-12 gap-2 relative"
-          >
-            <div className="col-span-12 md:col-span-6 flex items-center space-x-2">
-              <Image
-                alt="pImage"
-                src={thumbnail}
-                width={75}
-                height={75}
-                className="border rounded-lg"
-              />
-              <div className="flex flex-col space-y-2">
-                <h5 className="paragraph-m-16 text-typeHigh">{title}</h5>
-                <p className="paragraph-r-16 text-typeLow">{brand}</p>
-                <p className="paragraph-r-16 text-typeLow">Qty - 1</p>
-              </div>
-            </div>
-            <div className="col-span-4 md:col-span-2 paragraph-r-14 text-typeHigh">
-              <span className="md:hidden text-xs">price:</span> ${price}
-            </div>
-            <div className="col-span-4 md:col-span-2 paragraph-r-14 text-typeHigh">
-              <span className="md:hidden text-xs">Qty:</span> 1
-            </div>
-            <div className="col-span-4 md:col-span-2 paragraph-r-14 text-typeHigh">
-              <span className="md:hidden text-xs">subtotal:</span> ${price}
-            </div>
+      <>
+        {products.length > 0 ? (
+          <>
+            {products.map((prod, i) => {
+              const { title, brand, price, thumbnail } = prod;
+              return (
+                <div
+                  key={i}
+                  className="my-2 border-b mb-2 pb-10 md:pb-2 grid grid-cols-12 gap-2 relative"
+                >
+                  <div className="col-span-12 md:col-span-6 flex items-center space-x-2">
+                    <Image
+                      alt="pImage"
+                      src={thumbnail}
+                      width={75}
+                      height={75}
+                      className="border rounded-lg"
+                    />
+                    <div className="flex flex-col space-y-2">
+                      <h5 className="paragraph-m-16 text-typeHigh">{title}</h5>
+                      <p className="paragraph-r-16 text-typeLow">{brand}</p>
+                      <p className="paragraph-r-16 text-typeLow">Qty - 1</p>
+                    </div>
+                  </div>
+                  <div className="col-span-4 md:col-span-2 paragraph-r-14 text-typeHigh">
+                    <span className="md:hidden text-xs">price:</span> ${price}
+                  </div>
+                  <div className="col-span-4 md:col-span-2 paragraph-r-14 text-typeHigh">
+                    <span className="md:hidden text-xs">Qty:</span> 1
+                  </div>
+                  <div className="col-span-4 md:col-span-2 paragraph-r-14 text-typeHigh">
+                    <span className="md:hidden text-xs">subtotal:</span> $
+                    {price}
+                  </div>
 
-            <div className="absolute bottom-2 right-3 flex space-x-4 items-center">
-              <button className="text-primary underline paragraph-m-16 font-bold">
-                Move to Wishlist
-              </button>
-              <button
-                onClick={() => removeFromCart(prod)}
-                className="text-error underline paragraph-m-16 font-bold"
-              >
-                Remove
-              </button>
-            </div>
-          </div>
-        );
-      })}
+                  <div className="absolute bottom-2 right-3 flex space-x-4 items-center">
+                    <button className="text-primary underline paragraph-m-16 font-bold">
+                      Move to Wishlist
+                    </button>
+                    <button
+                      onClick={() => removeFromCart(prod)}
+                      className="text-error underline paragraph-m-16 font-bold"
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <p className="mt-6 text-error">Cart empty !</p>
+        )}
+      </>
     </div>
   );
 };
